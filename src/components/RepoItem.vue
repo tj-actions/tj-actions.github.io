dae0e8c33a70e87b1acd65706dd6df53f5aad1d6
Tonye Jack <jtonye@ymail.com>
2021-07-14 17:46:19 -0400

    Update RepoItem.vue

--- src/components/RepoItem.vue
+++ src/components/RepoItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="repo-item">
-    <a class="repo" v-bind:href="`https://github.com/tj-actions/{{ repoSha }}`" target="_blank">{{ repository }}</a>
-    <iframe v-bind:src="`https://ghbtns.com/github-btn.html?user=tj-actions&repo={{ repoSha }}&type=star&count=true&size=medium`" frameborder="0" scrolling="0" width="170" height="30" title="GitHub"></iframe>
+    <a class="repo" v-bind:href="`https://github.com/tj-actions/${repoSha}`" target="_blank">{{ repository }}</a>
+    <iframe v-bind:src="`https://ghbtns.com/github-btn.html?user=tj-actions&repo=${repoSha}&type=star&count=true&size=medium`" frameborder="0" scrolling="0" width="170" height="30" title="GitHub"></iframe>
   </div>
 </template>
 
